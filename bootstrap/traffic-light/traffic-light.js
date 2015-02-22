b403db9fd405c72abd4908eea29090c7516398fd
Marius <leatix@gmail.com>
2015-02-22 23:08:39 +0100

    First setup for template

---
+++ bootstrap/traffic-light/traffic-light.js
@@ -0,0 +1,72 @@
++function ($) {
+  'use strict';
+  
+  var TrafficLight = function (element, options) {
+    this.$element  = $(element)
+    this.options   = options
+    this.state     = options.state || "red"
+    
+    var images = ['top', 'off', 'middle', 'off', 'bottom'];
+    images[1] = (this.state == "red") ? "red" : "off";
+    images[3] = (this.state == "green") ? "green" : "off";
+    
+    $.each(images, function(k, v) {
+      $(element).append($('<div><img src="traffic-light/' + v + '.png" /></div>'));
+    });    
+  }
+  
+  TrafficLight.DEFAULTS = {
+  }
+  
+  TrafficLight.prototype.load = function() {
+    this.state = (this.state == "red") ? "green" : "red"
+  }
+  
+  TrafficLight.prototype.toggle = function () {   
+    if (this.state == "red") {    
+      this.$element.find('div:nth-child(2) img').replaceWith('<img src="traffic-light/red.png" />');
+      this.$element.find('div:nth-child(4) img').replaceWith('<img src="traffic-light/off.png" />');
+    } else {
+      this.$element.find('div:nth-child(2) img').replaceWith('<img src="traffic-light/off.png" />');
+      this.$element.find('div:nth-child(4) img').replaceWith('<img src="traffic-light/green.png" />');
+    }
+    
+    var toggleEvent = $.Event('toggle')
+    this.$element.trigger(toggleEvent, [ this.state ])
+    
+    this.state = (this.state == "red") ? "green" : "red"
+  }
+  
+  function Plugin(option) {
+    return this.each(function () {
+      var $this   = $(this)
+      var data    = $this.data('bs.trafficlight')
+      var options = typeof option == 'object' && option
+
+      if (!data) $this.data('bs.trafficlight', (data = new TrafficLight(this, options)))
+
+      if (option == 'toggle') data.toggle()
+      else if (option) data.load()
+    })
+  }
+  
+  var old = $.fn.trafficLight
+
+  $.fn.trafficLight             = Plugin
+  $.fn.trafficLight.Constructor = TrafficLight
+  
+  $(document)
+    .on('click.bs.trafficlight.data-api', 'div.traffic-light', function(e) {
+      var $trafficLight = $(e.target)
+      if (!$trafficLight.hasClass('traffic-light')) $trafficLight = $trafficLight.closest('.traffic-light')
+      Plugin.call($trafficLight, 'toggle')
+      e.preventDefault()
+    })
+  
+  $(window).on('load', function () {
+    $('div.traffic-light').each(function () {
+      var $trafficlight = $(this)
+      Plugin.call($trafficlight, $trafficlight.data())
+    })
+  })
+}(jQuery);
